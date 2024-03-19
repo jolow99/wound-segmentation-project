5d77148c6320295a77e62954b1672094f6801fe2
Joseph <jolow999@gmail.com>
2024-03-19 15:21:14 +0800

    It can run but can't train

---
+++ models/deeplab/modeling_deeplabv3plus.py
@@ -0,0 +1,77 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class ASPP(nn.Module):
+    def __init__(self, in_channels, atrous_rates):
+        super(ASPP, self).__init__()
+        out_channels = 256
+        self.convs = nn.ModuleList()
+
+        # 1x1 conv
+        self.convs.append(nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, 1, bias=False),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True)
+        ))
+
+        # Three 3x3 convs with different atrous_rates
+        for rate in atrous_rates:
+            self.convs.append(nn.Sequential(
+                nn.Conv2d(in_channels, out_channels, 3, padding=rate, dilation=rate, bias=False),
+                nn.BatchNorm2d(out_channels),
+                nn.ReLU(inplace=True)
+            ))
+
+        # Image pooling
+        self.image_pooling = nn.Sequential(
+            nn.AdaptiveAvgPool2d(1),
+            nn.Conv2d(in_channels, out_channels, 1, bias=False),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True)
+        )
+
+    def forward(self, x):
+        size = x.shape[-2:]
+        image_features = F.interpolate(self.image_pooling(x), size=size, mode='bilinear', align_corners=False)
+
+        aspp_features = [conv(x) for conv in self.convs]
+        aspp_features.append(image_features)
+        return torch.cat(aspp_features, dim=1)
+
+class DeepLabHead(nn.Module):
+    def __init__(self, in_channels, num_classes):
+        super(DeepLabHead, self).__init__()
+        self.aspp = ASPP(in_channels, [12, 24, 36])  # Atrous rates for an output stride of 16
+        aspp_out_channels = 5 * 256  # 5 branches in ASPP, each with 256 channels
+        self.conv = nn.Conv2d(aspp_out_channels, 256, 1, bias=False)
+        self.bn = nn.BatchNorm2d(256)
+        self.relu = nn.ReLU(inplace=True)
+        self.last_conv = nn.Conv2d(256, num_classes, 1)
+
+    def forward(self, x):
+        x = self.aspp(x)
+        x = self.conv(x)
+        x = self.bn(x)
+        x = self.relu(x)
+        x = self.last_conv(x)
+        return x
+
+
+
+# Assuming we are using output stride 16, we do not modify the backbone
+# However for output stride 8, we would need to apply modifications
+# to the backbone to have dilated convolutions in the last block
+
+# Construct the DeepLabV3+ model
+class DeepLabV3Plus(nn.Module):
+    def __init__(self, backbone, head):
+        super(DeepLabV3Plus, self).__init__()
+        self.backbone = backbone
+        self.head = head
+
+    def forward(self, x):
+        x = self.backbone(x)
+        x = self.head(x)
+        return F.interpolate(x, size=(224, 224), mode='bilinear', align_corners=False)
--- models/models.py
+++ models/models.py
@@ -1,12 +1,12 @@
 
 import sys 
-import torch
-import torchvision.models as models
+from torchvision import models
 
 sys.path.append('..')
 from models.unet.modeling_simple_unet import SimpleUNet
 from models.unet.configuration_unet import UNetConfig
 from models.unet.modeling_unet import UNet
+from models.deeplab.modeling_deeplabv3plus import DeepLabV3Plus, DeepLabHead
 
 def get_model(model_name, config, device="cuda"): 
     if model_name == 'unet': 
@@ -15,16 +15,17 @@ def get_model(model_name, config, device="cuda"):
     elif model_name == 'simple_unet':
         config = UNetConfig(**config)
         model = SimpleUNet(config).to(device)
-    elif model_name == 'mobilenet': 
-        # model = torch.hub.load('pytorch/vision:v0.10.0', 'mobilenet_v2', pretrained=True)
-        model = models.mobilenet_v2(weights=models.MobileNet_V2_Weights.IMAGENET1K_V1)
-        # Replace the last layer with a new convolutional layer
-        model.classifier = torch.nn.Conv2d(1280, 1, kernel_size=1)
+    elif model_name == 'deeplabv3plus': 
+        # Load a pretrained MobileNetV2 model
+        model = models.mobilenet_v2(pretrained=True)
 
-        # Replace the last layer of the features module with a regular convolutional layer
-        model.features[-1][0] = torch.nn.Conv2d(320, 1280, kernel_size=1, bias=False)
-        model.to(device)
-        
+        # Modify the MobileNetV2 model to replace the classifier with the DeepLab head
+        backbone = model.features
 
-        
+        # Create the DeepLab head with the number of classes, for example 21 for VOC
+        num_classes = 1
+        deeplab_head = DeepLabHead(1280, num_classes)  # 1280 is the number of output channels from MobileNetV2
+
+        model = DeepLabV3Plus(backbone, deeplab_head)
+        model = model.to(device)
     return model
