6770290d5c71aff09100124d6bfe3b7e08ba79a0
Joseph <jolow999@gmail.com>
2024-04-12 14:38:27 +0800

    add fcn

--- models/models.py
+++ models/models.py
@@ -5,6 +5,7 @@ from models.unet.modeling_simple_unet import SimpleUNet
 from models.unet.configuration_unet import UNetConfig
 from models.unet.modeling_unet import UNet
 from models.deeplab.modeling_deeplabv3plus import DeepLabV3Plus
+from models.fcn.modeling_fcn import FCN_Vgg16_16s
 
 #dont use _ for model names
 def get_model(model_name, config, device="cuda"): 
@@ -16,4 +17,6 @@ def get_model(model_name, config, device="cuda"):
         model = SimpleUNet(config).to(device)
     elif model_name == 'deeplabv3plus': 
         model = DeepLabV3Plus().to(device)
+    elif model_name == 'fcn': 
+        model = FCN_Vgg16_16s().to(device)
     return model
