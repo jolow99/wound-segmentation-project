737979220ff46b85be3b7e6e9ddaff0db5b9250b
Joseph <jolow999@gmail.com>
2024-04-12 17:30:48 +0800

    Add segnet

--- models/models.py
+++ models/models.py
@@ -7,7 +7,6 @@ from models.unet.modeling_unet import UNet
 from models.deeplab.modeling_deeplabv3plus import DeepLabV3Plus
 from models.fcn.modeling_fcn import FCN_Vgg16_16s
 from models.segnet.modeling_segnet import SegNet
-from models.fcn.modeling_fcn import FCN_Vgg16_16s
 
 #dont use _ for model names
 def get_model(model_name, config, device="cuda"): 
@@ -23,6 +22,4 @@ def get_model(model_name, config, device="cuda"):
         model = FCN_Vgg16_16s().to(device)
     elif model_name == 'segnet':
         model = SegNet().to(device)
-    elif model_name == 'fcn': 
-        model = FCN_Vgg16_16s().to(device)
     return model
